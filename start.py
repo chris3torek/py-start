7c76fcd4ee4da412983829f6a499d1f69708641c
Chris Torek <chris.torek@gmail.com>
2013-08-09 05:54:29 -0600

    start.py: oops! add "write to file"
    If we are not dumping a stack trace to a user, we should
    dump it to a file, or at least try to.

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 """start.py"""
 
-import errno, os, signal, sys, traceback
+import errno, os, signal, stat, sys, time, traceback
 
 def sigdie(sig):
     """Attempt to die from a signal.
@@ -26,15 +26,73 @@ def _split_err(err):
         return None, err, None
     return None, None, err
 
-def _show_err(err, prefix=None):
-    """Dump traceback from exc_info() error, but leave out the
-    first frame, which is start() itself, and do nothing if err
-    is None.
+def _open_tracedump_file(path):
+    """Open a trace-dump file (a la kernel core dump file).
+
+    Reject symlinks (if we have that option, at least).  Only
+    write regular files that have just one link.
     """
-    if err:
-        if prefix:
-            sys.stderr.write(prefix)
-        traceback.print_exception(err[0], err[1], err[2].tb_next)
+    nofollow = getattr(os, 'O_NOFOLLOW', 0)
+    if nofollow == 0:
+        # We don't have O_NOFOLLOW.  Racy test for symlink...
+        try:
+            stbuf = os.lstat(path)
+            if stat.S_ISLNK(stbuf.st_mode):
+                # EMLINK seems odd, but is what FreeBSD gives
+                # with real O_NOFOLLOW.
+                raise OSError(errno.EMLINK, 'too many links: %r' % path)
+        except OSError as err:
+            if err.errno != errno.ENOENT:
+                raise
+    fd = os.open(path, os.O_CREAT | os.O_WRONLY | os.O_APPEND | nofollow, 0600)
+    stbuf = os.fstat(fd)
+    if not stat.S_ISREG(stbuf.st_mode):
+        raise IOError(errno.EINVAL, 'not a regular file: %r' % path)
+    if stbuf.st_nlink > 1:
+        raise IOError(errno.EMLINK, 'too many links (%d): %r' %
+            (stbuf.st_nlink, path))
+    return fd
+
+def _print_tb(prefix, err, stream):
+    """Print given traceback to stream, with optional prefix
+    (if not None) and omitting one frame.  (See _show_err().)
+    """
+    if prefix:
+        stream.write(prefix)
+    traceback.print_exception(err[0], err[1], err[2].tb_next, None, stream)
+
+def _show_err(err, to_user, to_file, progname, prefix=None):
+    """Show info / dump traceback from exc_info() error, but leave
+    out the first frame, which is start() itself, and do nothing if
+    err is None.  Show this to user (stderr) if so directed, or if
+    not doing that, optionally, show into a "core dump".
+    """
+    if err is None:
+        return
+    if to_user:
+        _print_tb(prefix, err, sys.stderr)
+        return
+    if not to_file:
+        return
+    path = '%s.core' % progname # or use .trace, or ... ?
+    fd = -1
+    sys.stderr.write('internal error detected')
+    try:
+        fd = _open_tracedump_file(path)
+        stream = os.fdopen(fd, 'w')
+        stream.write('pid %d: %s\n' % (os.getpid(), time.ctime()))
+        _print_tb(prefix, err, stream)
+        stream.write('------------------------------\n')
+        stream.close()
+        fd = -1
+        sys.stderr.write(', trace in %r\n' % path)
+    except (OSError, IOError) as err2:
+        sys.stderr.write('\nunable to save stack trace: %s\n'
+            'set PYTHON_DEBUG=1 in environment'
+            ' to print stack trace to stderr\n' % err2)
+    finally:
+        if fd >= 0:
+            os.close(fd)
 
 def start(func, interrupt_trace=None, exc_trace=None):
     """
@@ -56,6 +114,13 @@ def start(func, interrupt_trace=None, exc_trace=None):
     back to a signal-style exit, so as to make this a well behaved
     Unix utility.
     """
+
+    if sys.argv:
+        progname = os.path.basename(sys.argv[0])
+        if progname.endswith('.py') and len(progname) > 3:
+            progname = progname[:-3]
+    else:
+        progname = 'python-script'
 
     if interrupt_trace is None:
         interrupt_trace = os.environ.get('PYTHON_SIGINT', False)
@@ -83,15 +148,16 @@ def start(func, interrupt_trace=None, exc_trace=None):
     pipe1, intr1, err1 = _split_err(err1)
     pipe2, intr2, err2 = _split_err(err2)
 
-    if interrupt_trace:
-        for err in (intr1, intr2):
-            _show_err(err)
-    if exc_trace:
-        for err in (pipe1, err1):
-            _show_err(err)
-        for err in (pipe2, err2):
-            # These have no traceback so say something first.
-            _show_err(err, 'In final sys.stdout.flush():\n')
+    _show_err(intr1, interrupt_trace, False, progname)
+    _show_err(pipe1, exc_trace, False, progname)
+    _show_err(err1, exc_trace, True, progname)
+
+    _show_err(intr2, interrupt_trace, False, progname)
+
+    # These have no traceback so say something first.
+    prefix = 'In final sys.stdout.flush():\n'
+    _show_err(pipe2, exc_trace, False, progname, prefix)
+    _show_err(err2, exc_trace, True, progname, prefix)
 
     if intr1 or intr2:
         ret = sigdie(signal.SIGINT)
